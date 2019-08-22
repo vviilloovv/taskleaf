dfe65bf2123bf35ed55344eda2c15416f46432af
vviilloovv <vviilloovv@gmail.com>
2019-08-22 10:09:51 +0900

    タスク検索共通処理を抽出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
@@ -17,14 +19,14 @@ class TasksController < ApplicationController
 
   def update
     task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
     task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   def create
@@ -39,5 +41,9 @@ class TasksController < ApplicationController
   private
     def task_params
       params.require(:task).recent
+    end
+
+    def set_task
+      @task = current_user.tasksfind(params[:id])
     end
 end

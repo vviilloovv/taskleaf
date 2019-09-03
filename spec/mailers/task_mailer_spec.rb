8150f2e3ca79bbff0921e775514cb9618bcb3d61
vviilloovv <vviilloovv@gmail.com>
2019-09-03 11:01:03 +0900

    メール送信テストケース作成

--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -1,5 +1,39 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe TaskMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:task) { FactoryBot.create(:task, name: "メイラーSpecを書く", description: "送信したメールの内容確認") }
+
+  let(:text_body) do
+    part = mail.body.parts.detect { |part| part.content_type == "text/plain; charset=UTF-8" }
+    part.body.raw_source
+  end
+  let(:html_body) do
+    part = mail.body.parts.detect { |part| part.content_type == "text/html; charset=UTF-8" }
+    part.body.raw_source
+  end
+
+  describe "#creation_email" do
+    let(:mail) { TaskMailer.creation_email(task) }
+
+    it "想定通りのメールが生成されている" do
+      # ヘッダ
+      expect(mail.subject).to eq("タスク作成完了メール")
+      expect(mail.to).to eq(["user@example.com"])
+      expect(mail.from).to eq(["taskleaf@example.com"])
+
+      # text形式の本文
+      expect(text_body).to match("以下のタスクを作成しました")
+      expect(text_body).to match("メイラーSpecを書く")
+      expect(text_body).to match("送信したメールの内容確認")
+
+      # html形式の本文
+      expect(html_body).to match("以下のタスクを作成しました")
+      expect(html_body).to match("メイラーSpecを書く")
+      expect(html_body).to match("送信したメールの内容確認")
+    end
+  end
 end

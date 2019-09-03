c920cbe60acd10b11b8aa24287404a1143dfc78f
vviilloovv <vviilloovv@gmail.com>
2019-09-03 14:42:29 +0900

    CSVエクスポート機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,16 +8,29 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w!name created_at!
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map { |attr| task.send(attr) }
+      end
+    end
+  end
+
   private
     def validate_name_not_including_comma
       errors.add(:name, "にカンマを含めることはできません") if name&.include?(',')
-    end
-
-    def ransackable_attributes(auth_object = nil)
-      %w!name created_at!
-    end
-
-    def ransackable_associations(auth_object = nil)
-      []
     end
 end

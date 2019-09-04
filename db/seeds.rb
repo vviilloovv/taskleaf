531403866bcf51a6a095cab03a91b59cb988734b
vviilloovv <vviilloovv@gmail.com>
2019-09-04 15:10:58 +0900

    テストデータシード作成!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  name: "admin",
+  admin: true,
+  password: "password",
+  password_confirmation: "password",
+end
